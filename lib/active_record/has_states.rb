bede707cb8125b597cbd9aab6f2c03276020a147
S. Brent Faulkner <brentf@unwwwired.net>
2008-09-05 21:25:11 -0400

    initial tests

--- lib/active_record/has_states.rb
+++ lib/active_record/has_states.rb
@@ -4,6 +4,9 @@ module ActiveRecord
       base.extend HookMethod
     end
 
+    # TODO: add to list of reserved words to eliminate trouble-making state names
+    RESERVED_WORDS = %w(new)
+    
     class StateMachine < Hash
       attr_reader :model, :column
       
@@ -34,10 +37,10 @@ module ActiveRecord
             return unless @to_state
             self.#{column} = @to_state
             begin
-              @transitioning = true
+              @#{column}_transition = true
               send(method)
             ensure
-              @transitioning = false
+              @#{column}_transition = false
             end
           end
           protected :#{event}_transition
@@ -49,14 +52,22 @@ module ActiveRecord
     
     module HookMethod
       def has_states(*names, &block)
-        include InstanceMethods unless self.respond_to?(:state_columns)
+        include InstanceMethods unless self.respond_to?(:state_machines)
 
         options = names.extract_options!
         column = options[:in] || 'state'
-        state_machine = state_machine_for(column)
+        state_machine = state_machine_for(column.to_s)
 
+        names = names.collect { |n| n.to_s }
+        reserved = names & RESERVED_WORDS
+        if reserved.size == 1
+          raise ArgumentError, "#{reserved.to_sentence} is a reserved word"
+        elsif reserved.size > 1
+          raise ArgumentError, "#{reserved.to_sentence} are reserved words"
+        end
+        
         names.each do |name|
-          state_machine[name.to_s] = {}
+          state_machine[name] = {}
           class_eval "def #{name}?; #{column} == '#{name}'; end", __FILE__, __LINE__
           class_eval "named_scope :#{name}, :conditions => { :#{column} => '#{name}' }", __FILE__, __LINE__
           define_callbacks "before_exit_#{name}", "after_exit_#{name}", "before_enter_#{name}", "after_enter_#{name}"
@@ -65,22 +76,35 @@ module ActiveRecord
         initial_state = names.first
         
         class_eval <<-INITIALIZE, __FILE__, __LINE__
-          before_create :before_enter_initial
-          after_create :after_enter_initial
+          before_create :before_initial_#{column}
+          after_create :after_initial_#{column}
           
-          def before_enter_initial
+          def before_initial_#{column}
+            self.#{column} = '#{initial_state}' if self.#{column}.blank?
             errors.add("#{column}", "does not have initial value of #{initial_state}") and return false unless #{column} == '#{initial_state}'
             callback("before_enter_#{initial_state}")
           end
+          protected :before_initial_#{column}
           
-          def after_enter_initial
+          def after_initial_#{column}
             callback("after_enter_#{initial_state}")
           end
+          protected :after_initial_#{column}
         INITIALIZE
 
         class_eval <<-HOOK, __FILE__, __LINE__
+          def detect_transition
+            @#{column}_transition ||=  if #{column}_changed?
+              @from_state = self.#{column}_was
+              @to_state = self.#{column}
+              raise RuntimeError, "invalid transition for #{column} from #{@from_state} to #{@to_state}" unless self.class.state_machine_for('#{column}')[@from_state][@to_state]
+              true
+            end
+          end
+          protected :detect_transition
+          
           def create_or_update_without_callbacks
-            return super unless @transitioning
+            return super unless detect_transition
             callback("before_exit_\#{@from_state}")
             callback("before_enter_\#{@to_state}")
             result = super
@@ -88,6 +112,7 @@ module ActiveRecord
             callback("after_enter_\#{@to_state}")
             result
           end
+          protected :create_or_update_without_callbacks
         HOOK
         
         state_machine.instance_eval(&block) if block_given?
--- test/has_states_test.rb
+++ test/has_states_test.rb
@@ -1,8 +1,159 @@
 require 'test/unit'
 
-class HasStatesTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+require 'rubygems'
+require 'active_record'
+
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require 'active_record/has_states'
+
+require File.dirname(__FILE__) + '/../init'
+
+class Test::Unit::TestCase
+  # def assert_queries(num = 1)
+  #   $query_count = 0
+  #   yield
+  # ensure
+  #   assert_equal num, $query_count, "#{$query_count} instead of #{num} queries were executed."
+  # end
+  # 
+  # def assert_no_queries(&block)
+  #   assert_queries(0, &block)
+  # end
+  
+protected
+  def setup_db
+    stdout = $stdout
+
+    ActiveRecord::Base.logger
+
+    # AR keeps printing annoying schema statements
+    $stdout = StringIO.new
+
+    ActiveRecord::Schema.define(:version => 1) do
+      create_table :tickets do |t|
+        t.column :type, :string
+        t.column :state, :string
+        t.column :other_state, :string
+        t.column :problem, :string
+        t.column :resolution, :string
+      end
+    end
+  ensure  
+    $stdout = stdout
+  end
+
+  def teardown_db
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.drop_table(table)
+    end
+  end
+
+  def create(klass, options = {})
+    klass.create({ :problem => 'this is a problem' }.merge(options))
+  end
+end
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memory:")
+
+class Ticket < ActiveRecord::Base
+  validates_presence_of :problem
+end
+
+class TicketWithState < Ticket 
+  has_states :open, :ignored, :active, :abandoned, :resolved
+end
+
+class TicketWithOtherState < Ticket
+  has_states :unassigned, :assigned, :in => :other_state
+end
+
+class TicketWithConcurrentStates < Ticket
+  has_states :open, :ignored, :active, :abandoned, :resolved
+  has_states :unassigned, :assigned, :in => :other_state
+end
+
+class BaseTest < Test::Unit::TestCase
+  def setup
+    setup_db
+  end
+
+  def teardown
+    teardown_db
+  end
+
+  def test_should_create
+    ticket = create(Ticket)
+    assert_not_nil ticket
+    assert ! ticket.new_record?, ticket.errors.full_messages.to_sentence
+  end
+end
+
+class StateTest < Test::Unit::TestCase
+  def setup
+    setup_db
+  end
+
+  def teardown
+    teardown_db
+  end
+
+  def test_should_create_with_state
+    ticket = create(TicketWithState)
+    assert_not_nil ticket
+    assert ! ticket.new_record?, ticket.errors.full_messages.to_sentence
+    assert ticket.open?
+    assert ! (ticket.ignored? || ticket.active? || ticket.abandoned? || ticket.resolved?)
+  end
+  
+  def test_should_not_create_with_incorrect_state
+    ticket = create(TicketWithState, :state => :resolved)
+    assert_not_nil ticket
+    assert ticket.new_record?
+    assert_not_nil ticket.errors.on(:state)
+  end
+  
+  def test_should_not_create_with_invalid_state
+    ticket = create(TicketWithState, :state => :invalid)
+    assert_not_nil ticket
+    assert ticket.new_record?
+    assert_not_nil ticket.errors.on(:state)
+  end
+end
+
+class OtherStateTest < Test::Unit::TestCase
+  def setup
+    setup_db
+  end
+
+  def teardown
+    teardown_db
+  end
+
+  def test_should_create_with_other_state
+    ticket = create(TicketWithOtherState)
+    assert_not_nil ticket
+    assert ! ticket.new_record?, ticket.errors.full_messages.to_sentence
+    assert ticket.unassigned?
+    assert ! ticket.assigned?
+  end
+end
+
+class ConcurrentStatesTest < Test::Unit::TestCase
+  def setup
+    setup_db
+  end
+
+  def teardown
+    teardown_db
+  end
+
+  def test_should_create_with_concurrent_states
+    ticket = create(TicketWithConcurrentStates)
+    assert_not_nil ticket
+    assert ! ticket.new_record?, ticket.errors.full_messages.to_sentence
+    assert ticket.open?
+    assert ! (ticket.ignored? || ticket.active? || ticket.abandoned? || ticket.resolved?)
+    assert ticket.unassigned?
+    assert ! ticket.assigned?
   end
 end
